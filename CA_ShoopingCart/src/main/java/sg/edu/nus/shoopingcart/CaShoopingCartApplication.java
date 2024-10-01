321299bd7206d0505b69aad14893072ff12a307f
csiowxy <charmaine_siow@yahoo.com.sg>
2024-10-01 16:15:33 +0800

    Testing
    Test here

--- CA_ShoopingCart/src/main/java/sg/edu/nus/shoopingcart/CaShoopingCartApplication.java
+++ CA_ShoopingCart/src/main/java/sg/edu/nus/shoopingcart/CaShoopingCartApplication.java
@@ -8,6 +8,7 @@ public class CaShoopingCartApplication {
 
 	public static void main(String[] args) {
 		System.out.print(false);
+		System.out.print(true);
 		SpringApplication.run(CaShoopingCartApplication.class, args);
 	}
 
