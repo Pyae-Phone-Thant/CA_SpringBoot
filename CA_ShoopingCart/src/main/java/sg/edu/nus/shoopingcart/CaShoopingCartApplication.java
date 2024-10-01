ca1d621f3941af1cd53db5b96f201c42a65474d2
Pyae Phone Thant <pyaephonethant21@gmail.com>
2024-10-01 15:34:03 +0800

    created base folder

---
+++ CA_ShoopingCart/src/main/java/sg/edu/nus/shoopingcart/CaShoopingCartApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.shoopingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaShoopingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaShoopingCartApplication.class, args);
+	}
+
+}
---
+++ CA_ShoopingCart/src/test/java/sg/edu/nus/shoopingcart/CaShoopingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.shoopingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaShoopingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
