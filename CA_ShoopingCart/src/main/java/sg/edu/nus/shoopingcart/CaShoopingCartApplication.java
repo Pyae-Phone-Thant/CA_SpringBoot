6a14d4ab29487cde876159be0198cd342be0ac1c
Pyae Phone Thant <pyaephonethant21@gmail.com>
2024-10-01 15:45:31 +0800

    changed sth.

--- CA_ShoopingCart/src/main/java/sg/edu/nus/shoopingcart/CaShoopingCartApplication.java
+++ CA_ShoopingCart/src/main/java/sg/edu/nus/shoopingcart/CaShoopingCartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaShoopingCartApplication {
 
 	public static void main(String[] args) {
+		System.out.print(false);
 		SpringApplication.run(CaShoopingCartApplication.class, args);
 	}
 
